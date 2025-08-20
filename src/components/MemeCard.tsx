29eb6faf29c003925dd245fcc4fb55e01affddb7
Saloni Jaiswal <salonijaiswal622@gmail.com>
2025-08-20 17:38:20 +0530

    added shimmer ui

--- src/components/MemeCard.tsx
+++ src/components/MemeCard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const MemeCard = ({details}) => {
     const {url, title} = details;
     return (
-        <div className={"border-black p-5 m-5 border rounded-lg"}>
+        <div className={"border-black p-5 m-5 border rounded-lg w-80 h-80"}>
            <img className='w-64 h-64' src={url} alt="meme"/>
            <p>{title}</p>
         </div>
